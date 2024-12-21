5db0e250295e2ce2e417258826c12f4ec222882c
Advet24 <tomaradvet1@gmail.com>
2024-12-21 23:04:36 +0530

    first

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter first number: "
+read num1
+
+echo "Enter second number: "
+read num2
+
+echo "Enter third number: "
+read num3
+
+sum=$((num1 + num2 + num3))
+
+echo "The sum of $num1, $num2 and $num3 is $sum"
